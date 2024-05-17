bf8e989f593a3dea26ebc7b573b35b864ec77375
João Victor Gabriel Rocha Barcelos <jv140604@gmail.com>
2024-05-16 23:51:54 -0300

    Create Estruturas.c
    Estrutura usada no código

---
+++ Estruturas.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//Estrutura registro
+typedef struct{
+int matricula;
+char curso[100];
+char nome[100];
+int disponibilidade;
+}registro;
+
+
+
+
